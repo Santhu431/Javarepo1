ddad0aaa19278d0efd6ceba04f1431df605a2782
Santhu431 <143609410+Santhu431@users.noreply.github.com>
2024-02-14 18:36:27 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
     # *.jar > /dev/null 2> /dev/null < /dev/null &
-    nohup java -jar contacts-app.jar > spring_boot.log 2>&1 &
+    nohup java -jar AWSCodeDeployDemo-0.0.1-SNAPSHOT.jar > spring_boot.log 2>&1 &
