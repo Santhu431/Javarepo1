98af18e5274ff5f14c06a0be92bc1feb281bf033
Santhu431 <143609410+Santhu431@users.noreply.github.com>
2024-02-14 18:24:30 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+    # *.jar > /dev/null 2> /dev/null < /dev/null &
+    nohup java -jar contacts-app.jar > spring_boot.log 2>&1 &
